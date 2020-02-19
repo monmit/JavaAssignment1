4347f0b7c07d1fb8c3ff14449a6f25492d144cb2
Monica Mittal <monica.mittal41@gmail.com>
2020-02-19 21:28:23 +0100

    corrected surcharge

--- src/Calculation.java
+++ src/Calculation.java
@@ -2,7 +2,6 @@ public class Calculation {
 
     final double ratePercent = (12.5/100);
     private double surcharge, finalPrice, rawTax, manufacturedTax, importedTax;
-    //private String raw, manufactured, imported;
 
     Test2 test = new Test2();
 
@@ -15,25 +14,24 @@ public class Calculation {
                     (2 * (test.getPrice() + (ratePercent * (test.getPrice() * test.getQuantity())))/100);
             finalPrice = test.getPrice() + manufacturedTax;
         } else if (test.getType().toLowerCase().equals("Imported")) {
-            importedTax = (10 * test.getPrice()/100);
-            //importedTax = ((10 * test.getPrice()/100)+ surcharge);
+            importedTax = ((10 * test.getPrice()/100)+ surchargeCalculationOnImportedType());
             finalPrice = test.getPrice() + importedTax;
         }
         System.out.println(finalPrice);
         return finalPrice;
 
     }
-    /*private double surchargeCalculationOnImportedType() {
-        if (test.getType()== imported && finalPrice <= 100) {
+    private double surchargeCalculationOnImportedType() {
+        if (test.getType().toLowerCase().equals("Imported") && finalPrice <= 100) {
             surcharge = 5;
-        } else if (test.getType()== imported && (finalPrice > 100 && finalPrice <= 200)) {
+        } else if (test.getType().toLowerCase().equals("Imported") && (finalPrice > 100 && finalPrice <= 200)) {
             surcharge = 10;
-        } else if (test.getType()== imported && finalPrice > 200) {
+        } else if (test.getType().toLowerCase().equals("Imported") && finalPrice > 200) {
             surcharge = ((5 * finalPrice) / 100);
         }
         System.out.println(surcharge);
         return surcharge;
-    }*/
+    }
 
     public double getFinalPrice() {
         return finalPrice;
