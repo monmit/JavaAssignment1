637db6ee98f4b78cd314143a2e86486845fb85d1
Monica Mittal <monica.mittal41@gmail.com>
2020-02-19 21:21:07 +0100

    Refactored Class2

--- src/Test2.java
+++ src/Test2.java
@@ -1,61 +1,54 @@
 import java.util.Scanner;
 
 public class Test2 {
-    private double surcharge, finalPrice, rawTax, manufacturedTax, importedTax;
 
-    public boolean yesOrNo;
-    private static double ratePercent = 12.5;
-    private String name, type;
+    //public boolean yesOrNo;
+    private String name;
+    private String type;
+    private String answer;
     private int price, quantity;
 
-     public boolean UserInputForItem() {
-         while (yesOrNo = true) {
-         Scanner scanner = new Scanner(System.in);
-         System.out.println("-name<" + scanner.next(name) + ">");
-         System.out.println("-price<" + scanner.nextInt(price) + ">");
-         System.out.println("-quantity<" + scanner.nextInt(quantity) + ">");
-         System.out.println("-type<" + scanner.next(type) + ">");
-         System.out.println("Do you want to enter details of any other item (y/n)");
-         yesOrNo = scanner.nextBoolean();
-         }
-            return yesOrNo;
-      }
-
-        public double calculateTaxLiability() {
-            String raw = null;
-            String manufactured = null;
-            String imported = null;
-            if (type == raw) {
-                rawTax = (ratePercent * (price * quantity)) / 100;
-                finalPrice = price + rawTax;
-            } else if (type == manufactured) {
-                manufacturedTax = ((ratePercent * (price * quantity)) / 100) + (2 * (price + (ratePercent * (price * quantity)) / 100)) / 100;
-                finalPrice = price + manufacturedTax;
-            } else if (type == imported) {
-                importedTax = ((10 * price) + surcharge);
-                finalPrice = price + importedTax;
-            }
-            return finalPrice;
-        }
-
-        public double surchargeCalculation() {
-            if (finalPrice <= 100) {
-                surcharge = 5;
-            } else if (finalPrice > 100 && finalPrice <= 200) {
-                surcharge = 10;
-            } else if (finalPrice > 200) {
-                surcharge = ((5 * finalPrice) / 100);
-            }
-            return surcharge;
-        }
-        public void printOnConsole(){
-            System.out.println(name);
-            System.out.println(price);
-            System.out.println(finalPrice);
-            System.out.println(rawTax);
-            System.out.println(manufacturedTax);
-            System.out.println(importedTax);
-        }
-
+    public void UserInputForItem() {
+        //while (yesOrNo = true) {
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter item name below");
+            name = scanner.next().toLowerCase();
+            System.out.println("-name<" + name + ">");
+            System.out.println("Enter item price below");
+            price = scanner.nextInt();
+            System.out.println("-price<" + price + ">");
+            System.out.println("Enter item quantity below");
+            quantity = scanner.nextInt();
+            System.out.println("-quantity<" + quantity + ">");
+            System.out.println("Enter item type below: choose between: Raw, Manufactured or Imported");
+            type = scanner.next().toLowerCase();
+            System.out.println("-type<" + type + ">");
+            System.out.println("Do you want to enter details of any other item (y/n)");
+            answer = scanner.next().toLowerCase();
+            //yesOrNo = scanner.nextBoolean();
+        } while (answer.equals("Yes"));
+        //return yesOrNo;
     }
 
+      public String getName() {
+          return name;
+      }
+
+      public String getType() {
+          return type;
+      }
+      // type can be passed Raw, manufactured, imported.
+      public void setType(String type) {
+          this.type = type;
+      }
+
+      public int getPrice() {
+          return price;
+      }
+
+      public int getQuantity() {
+          return quantity;
+      }
+
+}
