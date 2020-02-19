4b755ce2aca6a9ed23d059eb78aad779c67b4f0f
Monica Mittal <monica.mittal41@gmail.com>
2020-02-19 21:22:05 +0100

    Refactored Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+    Test2 test2 = new Test2();
+    Calculation calculate = new Calculation();
+    PrintOnConsole print = new PrintOnConsole();
+    test2.UserInputForItem();
+    test2.setType("Raw");
+    calculate.calculateTaxLiability();
+    print.printOnConsole();
+        System.out.println("Success");
 
     }
 }
