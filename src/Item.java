b85f34317ddef5bdf27b0dc22b62ffc306b910e0
monica-jftw <monica.mittal@justforthewin.com>
2020-02-20 16:40:52 +0100

    Item class added

---
+++ src/Item.java
@@ -0,0 +1,38 @@
+public class Item {
+    private int price, quantity;
+    private String name;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
