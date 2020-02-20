98f0d8f612f6dde1fb310bc7f66b21a9972df9a3
monica-jftw <monica.mittal@justforthewin.com>
2020-02-20 16:40:31 +0100

    uMain updated

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-    //Test1 test = new Test1();
-        Test2 test2 = new Test2();
-        test2.UserInputForItem();
-        test2.calculateTaxLiability();
-        test2.surchargeCalculation();
-        test2.printOnConsole();
-        System.out.println("success");
-=======
-    Test2 test2 = new Test2();
-    Calculation calculate = new Calculation();
-    PrintOnConsole print = new PrintOnConsole();
-    test2.UserInputForItem();
-    test2.setType("Raw");
-    calculate.calculateTaxLiability();
-    print.printOnConsole();
-        System.out.println("Success");
+                Item item = new Item();
+                Calculation calculate = new Calculation();
+                calculate.calculateTaxLiability(item);
+                ConsoleHandler inputOutput = new ConsoleHandler();
+                inputOutput.UserInputForItem(item);
+                inputOutput.printOnConsole(item, calculate);
+                System.out.println("Success");
 
->>>>>>> 2d989f607ee7bae5e602c63337c53fd1d387e015
-    }
+     }
 }
+
