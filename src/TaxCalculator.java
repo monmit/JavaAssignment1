22c9f2315b1bbb858fe249911d4ef9fd4a326ab9
Monica Mittal <monica.mittal41@gmail.com>
2020-02-22 14:19:36 +0100

    delete old class files

--- src/TaxCalculator.java
+++
@@ -1,7 +0,0 @@
-public abstract class TaxCalculator {
-    // is class ka object create nhi ho skta
-    double ratePercent;
-    private double finalPrice, taxLiability;
-
-    abstract void calculateTaxLiability(Item item);
-}
