c778850cf68aa97894876322989b12c6192debf4
Monica Mittal <monica.mittal41@gmail.com>
2020-02-22 14:14:58 +0100

    corrected looped main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-                Item item = new Item();
-                Calculation calculate = new Calculation();
-                calculate.calculateTaxLiability(item);
-                ConsoleHandler inputOutput = new ConsoleHandler();
-                inputOutput.UserInputForItem(item);
-                inputOutput.printOnConsole(item, calculate);
-                System.out.println("Success");
+        ConsoleHandler inputOutput = new ConsoleHandler();
+        boolean run = true;
+        while(run) {
+            Item item = inputOutput.userInputForItem();
+            inputOutput.printOnConsole(item);
+            run = shouldRunAgain();
+        }
+
 
      }
-}
 
+    private static boolean shouldRunAgain() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        if(input.trim().equalsIgnoreCase("n")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
